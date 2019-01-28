a8e5b7b8620dc8af059ad32fb888fde838a1056c
manobhavj4 <manobhavj4@gmail.com>
2019-01-28 21:40:33 +0530

    start project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import{ createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
+// import Card from './Card';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = 
+		 createStore(rootReducer, applyMiddleware(thunkMiddleware,logger))
+
+ReactDOM.render(<Provider store={store}>
+					<App />
+				</Provider>, document.getElementById('root'));
+serviceWorker.unregister();
